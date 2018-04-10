0251141d961ff5722ceb5ce060252ade45340628
POTHULA, SANDEEP RAJ SUDARSHAN <pothulas5121@d150-43341.uhcl.edu>
2018-04-09 19:09:01 -0500

    04/09

---
+++ build/Pods.build/Debug-iphoneos/Pods-LightBLU.build/DerivedSources/Pods_LightBLU_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_LightBLUVersionString[];
+ extern const double Pods_LightBLUVersionNumber;
+
+ const unsigned char Pods_LightBLUVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_LightBLU  PROJECT:Pods-1" "\n";
+ const double Pods_LightBLUVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Pods.build/Debug-iphoneos/Pods-LightBLUTests.build/DerivedSources/Pods_LightBLUTests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_LightBLUTestsVersionString[];
+ extern const double Pods_LightBLUTestsVersionNumber;
+
+ const unsigned char Pods_LightBLUTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_LightBLUTests  PROJECT:Pods-1" "\n";
+ const double Pods_LightBLUTestsVersionNumber __attribute__ ((used)) = (double)1.;
